4cd0a65eb24553e01beb0703bb7abda45da0dab4
Ganesh K <kganesk@gmail.com>
2022-03-23 16:01:12 -0400

    numpy issue

--- panda6.py
+++ panda6.py
@@ -1,6 +1,6 @@
-#import numpy as np
-#import pandas as pd
+import numpy as np
+import pandas as pd
 
-#s1 = pd.Series(["B", "a", "C"])
-#print(s1.sort_values())
+s1 = pd.Series(["B", "a", "C"])
+print(s1.sort_values())
 print("Hello World from python")
